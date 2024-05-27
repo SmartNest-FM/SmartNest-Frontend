0949d865459f25b74021273c247ebf92bcc393ea
FaridIzquierdo <89101074+FaridIzquierdo@users.noreply.github.com>
2024-05-26 22:36:15 -0500

    Implement screen activity 1

---
+++ lib/screens/activity1_screen.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/material.dart';
+import 'package:smartnest/config/theme/app_theme.dart';
+import 'package:smartnest/firebase_auth_project/firebase_auth_services.dart';
+import 'package:smartnest/screens/home_screen.dart';
+import 'package:smartnest/screens/main_screens/welcome_screen.dart';
+import 'package:smartnest/screens/percentage_screen.dart';
+import 'package:smartnest/screens/profile_screen.dart';
+import 'package:smartnest/screens/settings_screen.dart';
+import 'package:smartnest/widgets/button/button_activities.dart';
+
+class ActivitiesScreen1 extends StatefulWidget {
+  const ActivitiesScreen1({super.key});
+
+  @override
+  State<ActivitiesScreen1> createState() => _ActivitiesScreen1State();
+}
+
+class _ActivitiesScreen1State extends State<ActivitiesScreen1> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final FirebaseAuthServices _auth = FirebaseAuthServices();
+
+  Future<void> _signOut() async {
+    try {
+      await _auth.signOut();
+      if (!mounted) return;
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const WelcomeScreen()),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error al cerrar sesión: $e'),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text('Actividad 1', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.red,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const HomeScreen()),
+            );
+          },
+          iconSize: 40,
+          color: Colors.white,
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+              _scaffoldKey.currentState?.openDrawer();
+            },
+            iconSize: 40,
+            color: Colors.white,
+          )
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const UserAccountsDrawerHeader(
+              accountName: Text("José Perez Mansilla"),
+              accountEmail: Text(""),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('lib/img/img_user.jpg'),
+              ),
+              decoration: BoxDecoration(
+                color: Color(0xFF1D4F7C),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Perfil'),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ProfileScreen()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Inicio'),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.bar_chart),
+              title: const Text('Niveles'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.trending_up),
+              title: const Text('Progreso de aprendizaje'),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const PercentageScreen()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Configuración'),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SettingsScreen()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.exit_to_app),
+              title: const Text('Cerrar sesión'),
+              onTap: _signOut,
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              AppTheme.getColorThemes()[0],
+              AppTheme.getColorThemes()[6]
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Reproducir enunciado',
+                    style: TextStyle(fontSize: 18,color: Colors.white),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.play_arrow, color: Colors.blue),
+                    onPressed: () {
+                      // Aquí va la lógica para reproducir el enunciado
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Image.asset(
+                'lib/img/img_cat.png', // Reemplazar con tu imagen
+                width: 150,
+                height: 150,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                '¿Qué Animal es este?',
+                style: TextStyle(fontSize: 18,color: Colors.white),
+              ),
+              const SizedBox(height: 20),
+              ButtonActivities(
+                text: 'Perro',
+                onPressed: () {},
+              ),
+              const SizedBox(height: 10),
+              ButtonActivities(
+                text: 'Gato',
+                onPressed: () {},
+              ),
+              const SizedBox(height: 10),
+              ButtonActivities(
+                text: 'Loro',
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
